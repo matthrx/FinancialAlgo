c0c904a79c0e71e28c090ea1e5666d16aceb1bff
matthrx <matthieu_upmc@outlook.fr>
2019-09-30 01:37:17 +0200

    Second commit ---- Further analysis thanks to STOCHRSI/SAR analysis (coefficient found). Centralized code ok to use server as a proxy (deployment of python application on Digital Ocean)

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import requests
 import pandas
 from datetime import datetime
-import threading
-import time
+import socket
+import sys
+import json
+
 
 """
 Algorithm working with the London schedule (from 9am to 6pm)
@@ -13,9 +15,45 @@ markets = ["EURUSD", "EURGBP", "GBPUSD"]
 frequency = "15min"
 financial_array = pandas.DataFrame(dtype="float16", columns=[], index=markets)
 
+host = "127.0.0.1"
+port = 8081
 
 COEFFSUM = 2
 KEY_TO_USE = 0
+
+try:
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host,port))
+except socket.error:
+    print("It seems connection was refused ...")
+    sys.exit(1)
+
+
+def reach_server(url_format):
+    """
+
+    :param url_format: url to reach through our proxy
+    :return: json format of the response
+    """
+    global s
+    try:
+        s.send(bytes(url_format, "utf-8"))
+        full_answer = str()
+        print("Received an answer from the server")
+        resp = s.recv(4096)
+        while (True):
+            full_answer += resp.decode("utf-8")
+            if len(resp.decode("utf-8")) != 4096 or resp.decode("utf-8") == str():
+                break
+            resp = s.recv(4096)
+        print(json.loads(full_answer))
+        s.close()
+
+    except socket.error:
+        print("Server cannot answer...")
+        sys.exit(1)
+
+
 
 def get_MACD(currency):
     """
@@ -41,14 +79,29 @@ def get_STOCHRSI(currency):
     return STOCHRSI.json()['Technical Analysis: STOCHRSI'][time_n], STOCHRSI.json()['Technical Analysis: STOCHRSI'][time_n_1]
 
 
-def coeff_STOCHARSI(value_n_1, value_n):
+def coeff_STOCHARSI_SAR(value_n_1, value_n, SAR_relatif):
     """
-
+    SAR will change before the STOCHRSI
+    :param SAR_relatif : if SAR_relatif positive then SAR>open price => short othw long
     :param value_n_1: {%K & %D at n-1}
     :param value_n: {%K and %D at n}
     :return: tuple with the needed increment of buy coeff and sell coeff
     """
-
+    buy_coeff, sell_coeff = float(), float()
+    quit_overzone=1.5
+    FastD_n_1, FastK_n, FastK_n_1, FastD_n = \
+        float(value_n_1['FastD']), float(value_n['FastK']),float(value_n_1['FastK']), float(value_n["FastD"])
+    overbought_zone = FastK_n>=80 and FastK_n_1 >= 80
+    oversold_zone = FastK_n<=20 and FastK_n_1 <=20
+    if overbought_zone or oversold_zone:
+        return 0,0
+    if FastK_n > 20 and FastK_n_1 <= 20: return quit_overzone, sell_coeff
+    elif FastK_n_1 >= 80 and FastK_n < 80: return buy_coeff, quit_overzone
+    if FastD_n > 50 and FastK_n > 50 : buy_coeff += 0.25
+    else: sell_coeff += 0.25
+    if FastK_n_1<=FastD_n_1 and FastK_n>FastD_n and SAR_relatif < 0 : buy_coeff += 2
+    elif FastK_n_1>= FastD_n_1 and FastK_n<FastD_n and SAR_relatif > 0: sell_coeff += 2
+    return buy_coeff, sell_coeff
 
 def get_SAR(currency):
     """
@@ -75,17 +128,20 @@ def get_MOMENTUM(currency):
 
 
 
-if datetime.today().hour in range(9,18): # A coder par apport aux horaires de Londres
+if datetime.today().hour in range(0,24): # A coder par apport aux horaires de Londres
     buy_coeff, sell_coeff = float(),float()
     for i in range(len(markets)):
-        financial_data = requests.get("https://financialmodellingprep.com/api/v3/forex/GBPUSD"
+        financial_data = requests.get("https://financialmodellingprep.com/api/v3/forex/{}"
                                       .format(markets[i]))
         if financial_data.status_code == 200:
             open_price = financial_data.json()['open']
             financial_array.loc[markets[i], datetime.today().strftime("%d/%m/%y %H:%M")] = open_price
             SAR_relatif = float(get_SAR(markets[i])['SAR']) - float(open_price) #SAR relatif needs to be positive to indicate a long position (othw short)
+            # stoch_n, stoch_n_1 = get_STOCHRSI(markets[i])
+            # buy_coeff, sell_coeff = coeff_STOCHARSI_SAR(stoch_n_1, stoch_n, SAR_relatif)
+            print(buy_coeff, sell_coeff)
+            a = reach_server("https://www.alphavantage.co/query?function=MOM&symbol=EURGBP&interval=15min&time_period=10&series_type=close&apikey=")
 
-            #TO DO //
             # Add rsistochastique condition
 
 
