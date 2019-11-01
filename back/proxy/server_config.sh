3653523128c7ddb96b6012faf960380a7a4b373a
matthrx <49817172+matthrx@users.noreply.github.com>
2019-11-01 12:35:19 +0100

    Update server_config.sh

--- back/proxy/server_config.sh
+++ back/proxy/server_config.sh
@@ -2,4 +2,4 @@
 apt update
 apt -y install python3-pip build-essential libssl-dev libffi-dev python3-dev
 pip3 install requests
-python3 ~/FinancialAlgo/proxy/proxy.py
+python3 ~/FinancialAlgo/back/proxy/proxy.py
